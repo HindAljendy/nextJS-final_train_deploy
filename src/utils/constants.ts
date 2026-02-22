c48af5f0c5af96b53ddc9326369b634d8948ace4
HindAljendy <142920133+HindAljendy@users.noreply.github.com>
2026-02-23 02:54:01 +0800

    domain add

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,7 @@
 export const ARTICLE_PER_PAGE = 6;
 
 const DEVELOPMENT_DOMAIN = "http://localhost:3000";
+const PRODUCTION_DOMAIN = "https://next-js-final-train-deploy-z2vz.vercel.app";
 
-export const DOMAIN =  DEVELOPMENT_DOMAIN ; 
+export const DOMAIN = process.env.NODE_ENV === 'production' ? PRODUCTION_DOMAIN  : DEVELOPMENT_DOMAIN; 
 
